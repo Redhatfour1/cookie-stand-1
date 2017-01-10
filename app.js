98866023f18fcbe65c0ad78ae8c2a5d69d9fd37d
Allie Grampa <agrampa@yahoo.com>
2017-01-09 21:56:54 -0800

    Monday evening

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 'use strict';
 
+//var randomAndPurchased = function () // make a function to run the randomCustomers and simulatedPurchased functions?
+
 var times = ['6 a.m.:', '7 a.m.', '8 a.m.', '9 a.m.', '10 a.m.', '11 a.m.', '12 p.m.', '1 p.m.', '2 p.m.', '3 p.m.', '4 p.m.', '5 p.m.', '6 p.m.', '7 p.m.', '8 p.m.'];
 
 var firstPike = {
@@ -29,12 +31,6 @@ var firstPikeDaily = document.getElementById('firstPike');
 var sum = firstPikeHourly.reduce(function(a, b){
   return parseInt(a) + parseInt(b);  //hack from Adam to convert back to integers
 }, 0);
-// var seaTac.simulatedPurchased =ˆ document.getElementById('seaTac');
-// var seattleCenter.simulatedPurchased = document.getElementById('seattleCenter');
-// var capitolHill.simulatedPurchased = document.getElementById('capitolHill');
-// var alki.simulatedPurchased = document.getElementById('alki');
-
-//Get firstPike working, then apply to the other locations!
 
 for (var index = 0; index < firstPikeHourly.length; index++) {
   console.log(firstPikeHourly[index]);
@@ -47,77 +43,168 @@ for (var index = 0; index < firstPikeHourly.length; index++) {
 
 var totalListItem = document.createElement('li');
 listElement.textContent = 'Total: ' + sum + ' cookies';
-firstPikeDaily.appendChild(totalListItem);   //this is replacing 8p.m., not adding to it :(
+firstPikeDaily.appendChild(totalListItem);
 
 var seaTac = {
   minCust: 3,
   maxCust: 24,
   averageCookiePerSale: 1.2,
 
-  randomCustomers: function(minCust, maxCust) {
-    return Math.round(Math.random() * (this.maxCust - this.minCust) + this.minCust);
+  randomCustomers: function() {
+    return Math.round(Math.random() * ((this.maxCust - this.minCust) + this.minCust));
+    console.log(randomCustomers);
   },
 
   simulatedPurchased: function() {
-    array: [];
-    for(index = 0; index < 15; index++ ) {
-      this.averageCookiePerSale * this.randomCustomers;
-      array.push;
+    var hourlyArray = [];   //somehow this is turning into a string!  :(
+    for(index = 0; index <= times.length; index++) {
+      var cookiesPerHour = this.averageCookiePerSale * this.randomCustomers();
+      console.log(cookiesPerHour);
+      hourlyArray.push(parseInt(cookiesPerHour.toFixed(0))); //math.floor?
     }
+    console.log(hourlyArray);
+    return hourlyArray;
   }
-
 };
+
+var seaTacHourly = seaTac.simulatedPurchased();
+var seaTacDaily = document.getElementById('seaTac');
+var sum = seaTacHourly.reduce(function(a, b){
+  return parseInt(a) + parseInt(b);  //hack from Adam to convert back to integers
+}, 0);
+
+for (var index = 0; index < seaTacHourly.length; index++) {
+  console.log(seaTacHourly[index]);
+  var listElement = document.createElement('li');
+  //listElement.setAttribute('class', 'average-cookie-per-hour');
+  listElement.textContent = times[index] + ': ' + seaTacHourly[index] + ' cookies';
+  //listElement.textContent = 'Total: ' + sum + ' cookies';
+  seaTacDaily.appendChild(listElement);
+}
+
+var totalListItem = document.createElement('li');
+listElement.textContent = 'Total: ' + sum + ' cookies';
+seaTacDaily.appendChild(totalListItem);
 
 var seattleCenter = {
   minCust: 11,
   maxCust: 38,
   averageCookiePerSale: 3.7,
 
-  randomCustomers: function(minCust, maxCust) {
-    return Math.round(Math.random() * (this.maxCust - this.minCust) + this.minCust);
+  randomCustomers: function() {
+    return Math.round(Math.random() * ((this.maxCust - this.minCust) + this.minCust));
+    console.log(randomCustomers);
   },
 
   simulatedPurchased: function() {
-    array: [];
-    for (index = 0; index < 15; index++) {
-      this.averageCookiePerSale * this.randomCustomers;
-      array.push;
+    var hourlyArray = [];   //somehow this is turning into a string!  :(
+    for(index = 0; index <= times.length; index++) {
+      var cookiesPerHour = this.averageCookiePerSale * this.randomCustomers();
+      console.log(cookiesPerHour);
+      hourlyArray.push(parseInt(cookiesPerHour.toFixed(0))); //math.floor?
     }
+    console.log(hourlyArray);
+    return hourlyArray;
   }
 };
+
+var seattleCenterHourly = seattleCenter.simulatedPurchased();
+var seattleCenterDaily = document.getElementById('seattleCenter');
+var sum = seattleCenterHourly.reduce(function(a, b){
+  return parseInt(a) + parseInt(b);  //hack from Adam to convert back to integers
+}, 0);
+
+for (var index = 0; index < seattleCenterHourly.length; index++) {
+  console.log(seattleCenterHourly[index]);
+  var listElement = document.createElement('li');
+  //listElement.setAttribute('class', 'average-cookie-per-hour');
+  listElement.textContent = times[index] + ': ' + seattleCenterHourly[index] + ' cookies';
+  //listElement.textContent = 'Total: ' + sum + ' cookies';
+  seattleCenterDaily.appendChild(listElement);
+}
+
+var totalListItem = document.createElement('li');
+listElement.textContent = 'Total: ' + sum + ' cookies';
+seattleCenterDaily.appendChild(totalListItem);
 
 var capitolHill = {
   minCust: 20,
   maxCust: 38,
   averageCookiePerSale: 2.3,
 
-  randomCustomers: function(minCust, maxCust) {
-    return Math.round(Math.random() * (this.maxCust - this.minCust) + this.minCust);
+  randomCustomers: function() {
+    return Math.round(Math.random() * ((this.maxCust - this.minCust) + this.minCust));
+    console.log(randomCustomers);
   },
 
   simulatedPurchased: function() {
-    array: [];
-    for (index = 0; index < 15; index++) {
-      this.averageCookiePerSale * this.randomCustomers;
-      array.push;
+    var hourlyArray = [];   //somehow this is turning into a string!  :(
+    for(index = 0; index <= times.length; index++) {
+      var cookiesPerHour = this.averageCookiePerSale * this.randomCustomers();
+      console.log(cookiesPerHour);
+      hourlyArray.push(parseInt(cookiesPerHour.toFixed(0))); //math.floor?
     }
+    console.log(hourlyArray);
+    return hourlyArray;
   }
 };
+
+var capitolHillHourly = capitolHill.simulatedPurchased();
+var capitolHillDaily = document.getElementById('capitolHill');
+var sum = capitolHillHourly.reduce(function(a, b){
+  return parseInt(a) + parseInt(b);  //hack from Adam to convert back to integers
+}, 0);
+
+for (var index = 0; index < capitolHillHourly.length; index++) {
+  console.log(capitolHillHourly[index]);
+  var listElement = document.createElement('li');
+  //listElement.setAttribute('class', 'average-cookie-per-hour');
+  listElement.textContent = times[index] + ': ' + capitolHillHourly[index] + ' cookies';
+  //listElement.textContent = 'Total: ' + sum + ' cookies';
+  capitolHillDaily.appendChild(listElement);
+}
+
+var totalListItem = document.createElement('li');
+listElement.textContent = 'Total: ' + sum + ' cookies';
+capitolHillDaily.appendChild(totalListItem);
 
 var alki = {
   minCust: 2,
   maxCust: 16,
   averageCookiePerSale: 4.6,
 
-  randomCustomers: function(minCust, maxCust) {
-    return Math.round(Math.random() * (this.maxCust - this.minCust) + this.minCust);
+  randomCustomers: function() {
+    return Math.round(Math.random() * ((this.maxCust - this.minCust) + this.minCust));
+    console.log(randomCustomers);
   },
 
   simulatedPurchased: function() {
-    array: [];
-    for(index = 0; index < 15; index++) {
-      this.averageCookiePerSale * this.randomCustomers;
-      array.push;
+    var hourlyArray = [];   //somehow this is turning into a string!  :(
+    for(index = 0; index <= times.length; index++) {
+      var cookiesPerHour = this.averageCookiePerSale * this.randomCustomers();
+      console.log(cookiesPerHour);
+      hourlyArray.push(parseInt(cookiesPerHour.toFixed(0))); //math.floor?
     }
+    console.log(hourlyArray);
+    return hourlyArray;
   }
 };
+
+var alkiHourly = alki.simulatedPurchased();
+var alkiDaily = document.getElementById('alki');
+var sum = alkiHourly.reduce(function(a, b){
+  return parseInt(a) + parseInt(b);  //hack from Adam to convert back to integers
+}, 0);
+
+for (var index = 0; index < alkiHourly.length; index++) {
+  console.log(alkiHourly[index]);
+  var listElement = document.createElement('li');
+  //listElement.setAttribute('class', 'average-cookie-per-hour');
+  listElement.textContent = times[index] + ': ' + alkiHourly[index] + ' cookies';
+  //listElement.textContent = 'Total: ' + sum + ' cookies';
+  alkiDaily.appendChild(listElement);
+}
+
+var totalListItem = document.createElement('li');
+listElement.textContent = 'Total: ' + sum + ' cookies';
+alkiDaily.appendChild(totalListItem);
